7daa01e2c087063977e95704c84dd6d0f901255f
jingpei <jingjpei@gmail.com>
2015-10-08 17:03:25 -0700

    (feat) Add new routes in middleware for venmo and oauth

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -7,6 +7,8 @@ module.exports = function(app, express) {
   var tripRouter = express.Router();
   var userRouter = express.Router();
   var messageRouter = express.Router();
+  var authRouter = express.Router();
+  var paymentRouter = express.Router();
 
   app.use(logger('dev'));
   app.use(bodyParser.json());
@@ -17,9 +19,13 @@ module.exports = function(app, express) {
   app.use('/users', userRouter);
   app.use('/trips', tripRouter);
   app.use('/messages', messageRouter);
+  app.use('/oauth', authRouter);
+  app.use('/venmo', paymentRouter);
 
   require('../users/userRoutes.js')(userRouter);
   require('../trips/tripRoutes.js')(tripRouter);
   require('../messages/messageRoutes.js')(messageRouter);
+  require('../auth/authRoutes.js')(authRouter);
+  require('../payment/paymentRoutes.js')(paymentRouter);
 
 };
